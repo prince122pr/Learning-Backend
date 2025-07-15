38222054fd5f647e33d288a5c864646daf8b6200
Prince <princeku6675@gmail.com>
2025-07-15 11:38:45 +0530

    fourth commit

--- Day-5/src/db/db.js
+++ Day-5/src/db/db.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 
-const connectToDB = async() => {
-    mongoose.connect('mongodb+srv://prince7869:7zUceg597Ru0Pa0R@cluster0.szvn4ni.mongodb.net/Backend').then(()=>{
-        console.log('DB is connected successfully!');  
-    }).catch((e)=>{
-      console.log('Error: ', e);
-    })
-}
+require('dotenv').config();
+
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+    });
+
+    console.log('✅ MongoDB connected successfully!');
+  } catch (error) {
+    console.error('❌ MongoDB connection error:', error.message);
+    process.exit(1); // Optional: exit the app
+  }
+};
 
 module.exports = connectToDB;
+
